245090352687e55869b13717d62f6d3bbb867110
Hannarong Senachai (Tom) <hannarong.senachai@gmail.com>
2025-09-07 18:22:04 +0700

    test ci

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,7 @@ const config: Config = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   moduleFileExtensions: ['ts', 'js', 'json'],
-  transform: { '^.+\.(t|j)s$': 'ts-jest' },
+  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
   collectCoverageFrom: ['src/**/*.ts', '!src/main.ts', '!src/**/dto/*.ts', '!src/**/entities/*.ts'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'json-summary'],
---
+++ tests/smoke.spec.ts
@@ -0,0 +1,5 @@
+describe('smoke', () => {
+  it('ok', () => {
+    expect(1).toBe(1);
+  });
+});
